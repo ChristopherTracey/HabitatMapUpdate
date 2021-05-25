9de8a82921508ac904d8fc9272c67a8401d488c8
Christoper Tracey <ctracey@paconserve.org>
2021-05-24 21:31:12 -0400

    bringing protected lands in

--- scripts/wetland_extractor.r
+++ scripts/wetland_extractor.r
@@ -7,6 +7,9 @@ arc.check_product()
 wetland_modeled <- "T:/HabitatMapUpdate/HabitatMap_OtherLandcover.gdb/ModeledWetlands_2013_Pennsylvania"
 wetland_restorable <- "T:/HabitatMapUpdate/HabitatMap_OtherLandcover.gdb/ModeledRestorableWetlands_2013_Pennsylvania"
 
+serverPath <- paste("C:/Users/",Sys.getenv("USERNAME"),"/AppData/Roaming/ESRI/ArcGISPro/Favorites/StateLayers.Default.pgh-gis0.sde/",sep="")
+
+
 wetland_modeled <- arc.open(wetland_modeled)
 wetland_modeled <- arc.select(wetland_modeled)
 wetland_modeled_sf <- arc.data2sf(wetland_modeled)
@@ -14,3 +17,14 @@ wetland_modeled_sf <- arc.data2sf(wetland_modeled)
 wetland_restorable <- arc.open(wetland_restorable)
 wetland_restorable <- arc.select(wetland_restorable)
 wetland_restorable_sf <- arc.data2sf(wetland_restorable)
+
+
+Protect_State <- arc.open(paste(serverPath,"StateLayers.DBO.PALTA_State", sep=""))
+Protect_State <- arc.select(Protect_State)
+Protect_State <- arc.data2sf(Protect_State)
+
+ProtectWetland_State <- st_intersection(wetland_modeled_sf, Protect_State)
+
+
+
+StateLayers.DBO.PALTA_Federal
